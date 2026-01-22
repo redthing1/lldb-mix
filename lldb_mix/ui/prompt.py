3f56b0ca0ac51a410e3ab9779f809e32375dfc15
redthing1 <redthing1@alt.icu>
2026-01-22 13:38:53 -0800

    don't clear lldb's default prompt prefix/suffix

--- lldb_mix/ui/prompt.py
+++ lldb_mix/ui/prompt.py
@@ -3,8 +3,4 @@ from __future__ import annotations
 PROMPT_TEXT = "mix> "
 PROMPT_COMMANDS = (
     f'settings set prompt "{PROMPT_TEXT}"',
-    "settings set use-color false",
-    'settings set -f -- prompt-ansi-prefix ""',
-    'settings set -f -- prompt-ansi-suffix ""',
-    "settings set show-statusline false",
 )
